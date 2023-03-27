51dc24e5b8f2e66cc277c028882adb48fc47bafc
SeoJH27 <morninglibrary@gmail.com>
2023-03-27 22:13:24 +0900

    변수 정의

--- app/src/main/java/com/example/mysurvey/MainActivity.java
+++ app/src/main/java/com/example/mysurvey/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mysurvey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -23,7 +24,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 정의
+        {
+            response = (Button) findViewById(R.id.response);
+            btn_birth = (Button) findViewById(R.id.btn_birth);
+            name = (EditText) findViewById(R.id.name);
+            sex = (ToggleButton) findViewById(R.id.sex);
+            elementryschool = (RadioButton) findViewById(R.id.elementryschool);
+            middleschool = (RadioButton) findViewById(R.id.middleschool);
+            highschool = (RadioButton) findViewById(R.id.highschool);
+            university = (RadioButton) findViewById(R.id.university);
+            check = (CheckBox) findViewById(R.id.check);
+            rating = (RatingBar) findViewById(R.id.rating);
+        }
 
+        response.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+
+        });
 
     }
 }
